2fa80e894dc8c235eee02990f5a8ba78240a6830
ajaysheokand-as <ajaysheokand.as@gmail.com>
2024-07-11 16:32:23 +0530

    Worked on typing test

--- components/quizpage.tsx
+++ components/quizpage.tsx
@@ -222,6 +222,8 @@ const QuizPage = () => {
                   <option value={10}>10</option>
                   <option value={20}>20</option>
                   <option value={30}>30</option>
+                  <option value={50}>50</option>
+                  <option value={90}>90</option>
                 </select>
                 <button
                   type="submit"
